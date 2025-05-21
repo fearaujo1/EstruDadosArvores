9343d4752acc50d90e5cec0e2d461ac0eee1bdc0
FelipeMGAraujo <168040742+FelipeMGAraujo@users.noreply.github.com>
2025-05-20 21:58:45 -0300

    Metodo searchPreOrder

--- src/Arvore.java
+++ src/Arvore.java
@@ -5,4 +5,12 @@ public class Arvore {
         if (node == null) return 0;
         return 1 + contarNos(node.filhoEsquerdo) + contarNos(node.filhoDireito);
     }
+
+    public void searchPreOrder(No node) {
+        if (node != null) {
+            System.out.println(node.value + "  ");
+            searchPreOrder(node.filhoEsquerdo);
+            searchPreOrder(node.filhoDireito);
+        }
+    }
 }
