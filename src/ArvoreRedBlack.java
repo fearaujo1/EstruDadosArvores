6ced1d73e3f8491ffa3e79639b1a94bb2a6537aa
FelipeMGAraujo <168040742+FelipeMGAraujo@users.noreply.github.com>
2025-06-17 21:37:55 -0300

    leftRotate and rightRotate

--- src/ArvoreRedBlack.java
+++ src/ArvoreRedBlack.java
@@ -8,4 +8,34 @@ public class ArvoreRedBlack {
         NIL.filhoEsquerdo = NIL.filhoDireito = NIL.parent = null;
         root = NIL;
     }
+
+    private void leftRotate(NoRedBlack x) {
+        NoRedBlack y = x.filhoDireito;
+        x.filhoDireito = y.filhoEsquerdo;
+        if (y.filhoEsquerdo != NIL) y. filhoEsquerdo.parent = x;
+
+        y.parent = x.parent;
+
+        if(x.parent == null) root = y;
+        else if (x == x.parent.filhoEsquerdo) x.parent.filhoEsquerdo = y;
+        else x.parent.filhoDireito = y;
+
+        y.filhoEsquerdo = x;
+        x.parent = y;
+    }
+
+    private void rightRotate(NoRedBlack y) {
+        NoRedBlack x = y.filhoDireito;
+        y.filhoDireito = x.filhoEsquerdo;
+        if (x.filhoEsquerdo != NIL) x.filhoEsquerdo.parent = y;
+
+        x.parent = y.parent;
+
+        if(y.parent == null) root = x;
+        else if (y == y.parent.filhoEsquerdo) y.parent.filhoDireito = x;
+        else y.parent.filhoEsquerdo = x;
+
+        x.filhoDireito = y;
+        y.parent = x;
+    }
 }
